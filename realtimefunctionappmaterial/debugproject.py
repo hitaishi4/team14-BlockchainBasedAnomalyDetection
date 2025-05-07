74be0a670db888896e5c198d2eb0b84c8fefcb67
kwarrenwagner <kenjiwag@bu.edu>
2025-05-06 23:46:08 -0400

    Add files via upload

--- realtimefunctionappmaterial/debugproject.py
+++ realtimefunctionappmaterial/debugproject.py
@@ -7,22 +7,53 @@ import requests
 from azure.eventhub import EventHubProducerClient, EventData
 import azure.functions as func
 import openai
-
+import numpy as np
 #app = func.FunctionApp()
 
 # Event Hub connection
 #connection_str = os.getenv("EVENT_HUB_CONNECTION_STRING")
 #eventhub_name = "team14eventhub"
 
-
+coef_const = -4.0168
+coef_indegree = 0.0896
+coef_outdegree = 0.0859
+coef_in_btc = 0.0132
+coef_out_btc = 0.1708
+coef_total_btc = -0.0922
 
 # Set OpenAI API key
 #need to make sure no one sees this information
-openai.api_key = "insert_api_key"
-openai.organization = "insert_org_id"
+openai.api_key = "bam"
+openai.organization = "kerchow"
 
 
+def predict_using_coefficients(transaction_data):
+  # Extracting features from the generated data
+  indegree = transaction_data['indegree']
+  outdegree = transaction_data['outdegree']
+  in_btc = transaction_data['in_btc']
+  out_btc = transaction_data['out_btc']
+  total_btc = transaction_data['total_btc']
+  # Compute log-odds (z)
+  z = (coef_const + 
+      coef_indegree * indegree + 
+      coef_outdegree * outdegree + 
+      coef_in_btc * in_btc + 
+      coef_out_btc * out_btc + 
+      coef_total_btc * total_btc)
 
+  # Apply sigmoid function to get the probability
+  probability = 1 / (1 + np.exp(-z))  # Sigmoid function
+
+  # Predict the class (1 for malicious, 0 for non-malicious)
+  prediction = 1 if probability >= 0.5 else 0
+
+  if prediction == 1:
+      print("Malicious transaction detected")
+      return "Malicious"
+  else:
+      print("Non-malicious")
+      return "Non-malicious"
 
 def generate_transaction_data():
     prompt = """
@@ -32,10 +63,10 @@ def generate_transaction_data():
     indegree: Number of transactions that are inputs of tx_hash
     outdegree: Number of transactions that are outputs of tx_hash.
     in_btc: Total number of bitcoins on each incoming edge to tx_hash.
-    out_btc: Total number of bitcoins on each outgoing edge from tx_hash.
-    total_btc: Net number of bitcoins flowing in and out from tx_hash.
-    mean_in_btc: Average number of bitcoins flowing in for tx_hash.
-    mean_out_btc: Average number of bitcoins flowing out for tx_hash.
+    out_btc: Total number of bitcoins on each outgoing edge from tx_hash. in _btc >= out_btc
+    total_btc: in_btc + out_btc.
+    mean_in_btc (type double): in_btc / indegree.
+    mean_out_btc (type double): out_btc / outdegree.
 
     Respond only with a JSON object.
     """
@@ -55,9 +86,11 @@ def main():
     est_time = utc_time.astimezone(pytz.timezone('America/New_York')).isoformat()
 
     order_data = generate_transaction_data()
+    prediction = predict_using_coefficients(order_data)
     data = {
             "timestamp": est_time,
-            "orders": order_data
+            "orders": order_data,
+            "prediction": prediction
     }
     print(data)
     with open("data.json", "w") as f:
